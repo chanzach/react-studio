6a52858a3e5b31bc615363f8a4f6502543cb58bb
ZB <themadhatter252@gmail.com>
2022-11-10 23:10:14 -0500

    final

--- src/App.js
+++ src/App.js
@@ -11,21 +11,43 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [total, setTotal] = useState(0);
+
+  const [list, setList] = useState([]);
+  
+  function clickHandler(price, name){
+    let newList = [...list, name]
+    setList(newList);
+    setTotal(total + price);
+  }
 
   return (
+    <>
+    <h1>Dom Dolla Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+      <div class = "flex-container">
+        {bakeryData.map((item) => ( // TODO: map bakeryData to BakeryItem components
+          <p>
+            <h3>{item.name}</h3>
+            <p>{item.description}</p>
+            <img src = {item.image} width={300} height={250}></img>
+            <button onClick = {() => clickHandler(item.price, item.name)}> Add to Cart</button>
+          </p> // replace with BakeryItem component
+        ))}
+      </div>
+      <div class = "flex-containter2">
+        <div>
+          <h2>Cart</h2>
+          {total == 0
+          ?<p>You have nothing in your cart.</p>
+          :<p> 
+            <p>Total: {total}</p> 
+            <p>{list.map((item)=> <ul>{item}</ul>)}</p> 
+          </p>}
+        </div>
       </div>
     </div>
-  );
+  </>);
 }
 
 export default App;
